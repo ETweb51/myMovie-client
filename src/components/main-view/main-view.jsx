3b404245a1f532ba80d9c9d1093d6c1fb72553e8
ETweb51 <webdev.et@gmail.com>
2021-10-18 22:29:16 +0200

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,38 +10,53 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-             ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'},
-                { _id: 2, Title: 'Ready Player One', Description: 'When the creator of a virtual reality called the OASIS dies, he makes a posthumous challenge to all OASIS users to find his Easter Egg, which will give the finder his fortune and control of his world.',
-             ImagePath: 'https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png'},
-                { _id: 3, Title: 'Private James Ryan', Description: 'Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.', 
-             ImagePath: 'https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg'}
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         };
     }
 
-    setSelectedMovie(newSelectedMovie) {
+    componentDidMount() {
+        axios.get('https://mymoviedbcf.herokuapp.com/movies')
+        .then(response => {
+            this.setState({
+                movies: response.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    setSelectedMovie(movie) {
         this.setState({
-            selectedMovie: newSelectedMovie
+            selectedMovie: movie
+        });
+    }
+
+    onLoggedIn(user) {
+        this.setState({
+            user
         });
     }
 
     render() {
-        const {movies, selectedMovie} = this.state;
-        if (selectedMovie) return <MovieView movie={selectedMovie} />
-        if (movies.length === 0){
-            return <div className="main-view">The list is empty mate!</div>;
-        } else {
-            return (
-                <div className="main-view">
-                    {selectedMovie ? 
-                    <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                    : movies.map(movie => ( <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>))}
-                </div>
-            );
-        }
+        const { movies, selectedMovie } = this.state;
+
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+    
+        if (movies.length === 0) return <div className="main-view" />;
+    
+        return (
+          <div className="main-view">
+            {selectedMovie
+              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+              : movies.map(movie => (
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+             ))
+            }
+          </div>
+        );
     }
 }
 
